8d7c6cc124ace180e735ef63184c0c3ddc1fa247
alexandredorc <ddorcival@gmail.com>
2022-10-24 02:35:49 +0200

    navigation exploration 1

---
+++ src/explore.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import rospkg
+import rospy
+import math
+import numpy as np
+
+class Frontier:
+    def __init__(self):
+        pass
+        
+if __name__ =="__main__":
+    rospy.init_node()
+    Frontier()
